8575f3894a0401517ea058490148b7a35c2a58b9
Laetaveinn <Xierfrogs@gmail.com>
2022-01-23 23:46:50 -0500

    setup overall test, fixed bug with medium attack bonus, removed redundant tests

--- Core/Char/MediumBaseAttackStrategy.cs
+++ Core/Char/MediumBaseAttackStrategy.cs
@@ -6,6 +6,9 @@ public class MediumBaseAttackStrategy : IBaseAttackStrategy
 {
     public int Levelup(int level)
     {
-        return (int) Math.Floor((decimal) ((level / 4) * 3));
+        //return (int) Math.Floor((decimal) ((level / 4) * 3));
+        var divlevel = level / 4.0;
+        var multiplied = divlevel * 3;
+        return (int)multiplied;
     }
 }
--- Test/MediumBaseAttackBonusTest.cs
+++ Test/MediumBaseAttackBonusTest.cs
@@ -1,31 +1,26 @@
 ﻿using Core;
 using NUnit.Framework;
 using Shouldly;
+using System;
 
 namespace Test
 {
     internal class MediumBaseAttackBonusTest
     {
         [Test]
-        public void BaseAttackBonus_AtFirstLevel_IsZero()
+        public void BaseAttackBonus_testalllevels()
         {
             var medium = new MediumBaseAttackStrategy();
 
-            medium.Levelup(1).ShouldBe(0);
-        }
-        [Test]
-        public void BaseAttackBonus_AtTenthLevel_IsSeven()
-        {
-            var medium = new MediumBaseAttackStrategy();
+            var expeccy = new[] { 0, 1, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 9, 10, 11, 12, 12, 13, 14, 15 };
 
-            medium.Levelup(10).ShouldBe(7);
-        }
-        [Test]
-        public void BaseAttackBonus_Attwentiethlevel_Isfifteen()
-        {
-            var medium = new MediumBaseAttackStrategy();
+            for (int i = 0; i < 20; i++)
+            {
 
-            medium.Levelup(20).ShouldBe(15);
+                medium.Levelup(i + 1).ShouldBe(expeccy[i]);
+                Console.WriteLine(medium.Levelup(i + 1));
+            }
+
         }
     }
 }
